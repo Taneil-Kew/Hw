5ccece488817977e9b03aea9d706c98962fb317c
Kew Forest Teacher <daronson@kewforest.org>
2018-05-14 22:17:31 -0400

    new

---
+++ Chapter 12/12.6.py
@@ -0,0 +1,8 @@
+import this
+#namespace are a great idea
+s = "If we took the bones out, it wouldn't be crunchy, would it?"
+s.split()
+type(s.split())
+s.split("o")
+s.split("i")
+"0".join(s.split("o"))
